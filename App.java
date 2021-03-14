130cfe482353e57fe37944ed3ce6cb6c345f3825
Keremenci <mkmenemencioglu@hotmail.com>
2021-03-14 21:55:27 +0300

    Added prints

--- App.java
+++ App.java
@@ -9,5 +9,9 @@ public class App {
 		int avg2 = Arrays.stream(pt2).average();
 		int avg3 = Arrays.stream(pt3).average();
 		int avg4 = Arrays.stream(pt4).average();
+		System.out.printf("Slice 1 [%d:%d] has an average of %d ", 0, idx1, avg1);
+		System.out.printf("Slice 2 [%d:%d] has an average of %d ", idx1, idx2, avg2);
+		System.out.printf("Slice 3 [%d:%d] has an average of %d ", idx2, idx3, avg3);
+		System.out.printf("Slice 4 [%d:%d] has an average of %d ", idx3, arr.length, avg4);
 	}
 }
